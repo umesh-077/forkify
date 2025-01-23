3783c728abb49507527ef5c299dd198343ea81eb
umesh-077 <gaikwadumesh261@gmail.com>
2025-01-23 13:15:04 +0530

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'c394b411-d1ce-430e-911d-feca5e922fd5';
+export const MODAL_CLOSE_SEC = 2.5;
