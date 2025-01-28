834bc4b81c1592050fb41ce12498feafffd45508
umesh-077 <gaikwadumesh261@gmail.com>
2025-01-28 13:09:54 +0530

    changes in data

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '316f4574-0038-4238-bf2d-5a3849572593';
+export const KEY = 'ccb6f8b9-c036-437c-9ae1-5e6de7806003';
 export const MODAL_CLOSE_SEC = 2.5;
